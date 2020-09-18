ddced6750fcb36559a80652d15876c243874f9c5
MattOmon <matthew.omondang160351@my.lsgh.edu.ph>
2020-09-19 00:27:35 +0800

    3 files modified, 1 added

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -3,12 +3,18 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  styleUrls: ['home.css'],
+
 })
 export class HomePage {
+  initValue: number;
+  depValue: number;
 
   constructor(public navCtrl: NavController) {
-
   }
 
+compute() {
+  this.depValue = (this.initValue * 0.40);
+}
 }
